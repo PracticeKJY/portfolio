179ac6d01a841fa50c3e14f7bb6b84254681d7e4
PracticeKJY <poiijtl1004@naver.com>
2023-07-15 13:48:28 +0900

    ✨ feat. 다크모드 추가 - SwitchButton.tsx  토글스위치버튼 컴포넌트 제작 - DarkModeButton.tsx 다크모드버튼 제작

--- app/component/DarkModeButton.tsx
+++ app/component/DarkModeButton.tsx
@@ -1,39 +1,15 @@
 "use client";
 
 import { useTheme } from "next-themes";
-import { useEffect, useState } from "react";
+import SwitchButton from "./SwitchButton";
 
 const DarkModeButton = () => {
   const { theme, setTheme } = useTheme();
-  const [mounted, setMounted] = useState(false);
-
-  useEffect(() => {
-    setMounted(true);
-  }, []);
-
-  if (!mounted) {
-    return null;
-  }
   return (
     <>
-      <button
-        className="
-              inline-flex items-center
-               border-0 py-1 px-3 rounded text-base mt-4 md:mt-0
-                focus:outline-none
-                bg-gray-100
-                hover:bg-gray-50
-                hover:text-orange-500
-                dark:bg-slate-600
-                dark:text-slate-400
-                dark:hover:bg-slate-700
-                dark:hover:text-yellow-300
-                "
-        type="button"
-        onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
-      >
-        {theme === "light" ? <LightModeSvg /> : <DarkModeSvg />}
-      </button>
+      <SwitchButton>
+        {theme === "dark" ? <LightModeSvg /> : <DarkModeSvg />}
+      </SwitchButton>
     </>
   );
 };
@@ -46,7 +22,7 @@ const LightModeSvg = () => {
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 24 24"
       fill="currentColor"
-      className="w-6 h-6"
+      className="absolute top-1 left-1 w-5 h-5 text-orange-400"
     >
       <path d="M12 2.25a.75.75 0 01.75.75v2.25a.75.75 0 01-1.5 0V3a.75.75 0 01.75-.75zM7.5 12a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM18.894 6.166a.75.75 0 00-1.06-1.06l-1.591 1.59a.75.75 0 101.06 1.061l1.591-1.59zM21.75 12a.75.75 0 01-.75.75h-2.25a.75.75 0 010-1.5H21a.75.75 0 01.75.75zM17.834 18.894a.75.75 0 001.06-1.06l-1.59-1.591a.75.75 0 10-1.061 1.06l1.59 1.591zM12 18a.75.75 0 01.75.75V21a.75.75 0 01-1.5 0v-2.25A.75.75 0 0112 18zM7.758 17.303a.75.75 0 00-1.061-1.06l-1.591 1.59a.75.75 0 001.06 1.061l1.591-1.59zM6 12a.75.75 0 01-.75.75H3a.75.75 0 010-1.5h2.25A.75.75 0 016 12zM6.697 7.757a.75.75 0 001.06-1.06l-1.59-1.591a.75.75 0 00-1.061 1.06l1.59 1.591z" />
     </svg>
@@ -58,7 +34,7 @@ const DarkModeSvg = () => {
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 24 24"
       fill="currentColor"
-      className="w-6 h-6"
+      className="absolute top-1 right-1 w-5 h-5 text-yellow-400"
     >
       <path
         fillRule="evenodd"
---
+++ app/component/SwitchButton.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import { FC, ReactNode } from "react";
+import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
+
+interface SwitchButtonProps {
+  children?: ReactNode;
+}
+
+const SwitchButton: FC<SwitchButtonProps> = ({ children }) => {
+  const { theme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return null;
+  }
+
+  return (
+    <label className="relative inline-flex items-center cursor-pointer">
+      <input type="checkbox" value="" className="sr-only peer" />
+      <div
+        className="w-14 h-7 bg-slate-300 peer-focus:outline-none peer-focus:ring-4 
+      peer-focus:ring-blue-300 dark:peer-focus:ring-blue-300 rounded-full peer
+      peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] 
+      after:absolute after:top-0.25 after:left-[0px] after:bg-white after:border-gray-300 
+      after:border after:rounded-full after:h-7 after:w-7 after:transition-all dark:border-gray-600
+      peer-checked:bg-yellow-200"
+        onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+      >
+        {children}
+      </div>
+    </label>
+  );
+};
+
+export default SwitchButton;
