81282061a6ffdceec7c55f66e376ef1bb4a8633a
PracticeKJY <poiijtl1004@naver.com>
2023-07-21 10:46:10 +0900

    🎨 modify. Body 부분 텍스트 크기 수정 및 이미지 수정

--- app/component/Body.tsx
+++ app/component/Body.tsx
@@ -20,37 +20,26 @@ const Body = ({ skillRef }: any) => {
         <HomeBackground />
         <div className="container mx-auto flex px-5 py-12 flex-col items-center">
           <div className="sticky  justify-center items-center lg:flex-grow   flex flex-col  mb-16 md:mb-0 text-center">
-            <h1 className="dark:text-white sm:text-3xl text-2xl mb-4 font-medium text-gray-900 ">
+            <h1 className="dark:text-white sm:text-[52px] sm:leading-[70px] text-2xl/10 mb-4 font-medium text-gray-900 ">
               프론트엔드 개발자
               <br />
-              <span className="sm:text-4xl text-3xl font-semibold">
-                김종엽
-              </span>{" "}
-              입니다.
-              <br />
+              <div className="relative inline-block">
+                <span className="underline-neon sm:text-[60px] text-3xl font-semibold">
+                  김종엽
+                </span>{" "}
+                입니다. <br />
+              </div>
             </h1>
-            <div className="pt-10 hidden sm:inline-block text-xl ">
-              <div>
-                <span className="inline-block relative after:content-['_'] after:inline-block after:absolute after:w-full after:h-[6px] after:bg-purple-200 after:bottom-0 after:left-0">
-                  새로운 지식
-                </span>
-                을 학습하고{" "}
-                <span className="underline-neon">
-                  만드는 것
-                </span>
-                을 좋아합니다.
+            <div className="pt-10 font-xl sm:text-[28px] sm:leading-[50px] ">
+              <div className="">
+                <span>새로운 지식</span>을 학습하고{" "}
+                <span>만드는 것</span>을 좋아합니다.
               </div>
               <div>
-                <span className="underline-neon">
-                  개인보다 팀
-                </span>
-                을 우선시 합니다.
+                <span>개인보다 팀</span>을 우선시 합니다.
               </div>
               <div>
-                <span className="underline-neon">
-                  How보다 Why
-                </span>
-                를 고민합니다.
+                <span>How보다 Why</span>를 고민합니다.
               </div>
             </div>
             <div
