36e4ab4120c5c0c3437dcc1f45a2fcf531472b5f
codespace-jake <119389577+PracticeKJY@users.noreply.github.com>
2024-08-27 22:19:35 +0900

    modify. yaer 부분 수정

--- app/component/Footer.tsx
+++ app/component/Footer.tsx
@@ -1,17 +1,20 @@
+"use client"
+
 const Footer = () => {
+  const year = new Date().getFullYear()
   return (
     <>
       <footer className="text-gray-600 body-font">
         <div className="bg-primary">
           <div className="container px-5 py-6 mx-auto flex justify-center items-center">
             <p className="text-sm text-gray-500  sm:mt-0">
-              © 2023 - 김종엽 all rights reserved.
+              © {year} - kim jong yeop all rights reserved.
             </p>
           </div>
         </div>
       </footer>
     </>
-  );
-};
+  )
+}
 
-export default Footer;
+export default Footer
